TelegramPlayground/PyroTGFork

commit b523632f1f6f2d2e76e9188dd8f0dc0d309cbcab

Add video_cover and video_start_timestamp to copy_message - Fix bug in commit 4fc8594

pyrogram/types/messages_and_media/message.py
@@ -4679,6 +4679,8 @@ class Message(Object, Update):
         parse_mode: Optional["enums.ParseMode"] = None,
         caption_entities: list["types.MessageEntity"] = None,
         show_caption_above_media: bool = None,
+        video_cover: Optional[Union[str, "io.BytesIO"]] = None,
+        video_start_timestamp: int = None,
         disable_notification: bool = None,
         reply_parameters: "types.ReplyParameters" = None,
         reply_markup: Union[
@@ -4732,6 +4734,12 @@ class Message(Object, Update):
 
             show_caption_above_media (``bool``, *optional*):
                 Pass True, if the caption must be shown above the message media. Ignored if a new caption isn't specified.
+
+            video_cover (``str`` | :obj:`io.BytesIO`, *optional*):
+                New cover for the copied video in the message. Pass None to skip cover uploading and use the existing cover.
+            
+            video_start_timestamp (``int``, *optional*):
+                New start timestamp, from which the video playing must start, in seconds for the copied video in the message.
 
             disable_notification (``bool``, *optional*):
                 Sends the message silently.
@@ -4827,7 +4835,29 @@ class Message(Object, Update):
             elif self.document:
                 file_id = self.document.file_id
             elif self.video:
-                file_id = self.video.file_id
+                return await self._client.send_video(
+                    chat_id,
+                    video=self.video.file_id,
+                    caption=caption,
+                    parse_mode=parse_mode,
+                    caption_entities=caption_entities,
+                    show_caption_above_media=show_caption_above_media or self.show_caption_above_media,
+                    cover=video_cover,
+                    start_timestamp=video_start_timestamp,
+                    has_spoiler=self.has_media_spoiler,
+                    disable_notification=disable_notification,
+                    protect_content=self.has_protected_content if protect_content is None else protect_content,
+                    allow_paid_broadcast=allow_paid_broadcast,
+                    message_thread_id=self.message_thread_id if message_thread_id is None else message_thread_id,
+                    business_connection_id=self.business_connection_id if business_connection_id is None else business_connection_id,
+                    send_as=send_as,
+                    message_effect_id=self.effect_id,
+                    reply_parameters=reply_parameters,
+                    reply_markup=self.reply_markup if reply_markup is object else reply_markup,
+                    # TODO
+                    schedule_date=schedule_date,
+                    reply_to_message_id=reply_to_message_id
+                )
             elif self.animation:
                 file_id = self.animation.file_id
             elif self.voice:

pyrogram/methods/messages/copy_message.py
@@ -36,6 +36,8 @@ class CopyMessage:
         parse_mode: Optional["enums.ParseMode"] = None,
         caption_entities: list["types.MessageEntity"] = None,
         show_caption_above_media: bool = None,
+        video_cover: Optional[Union[str, "io.BytesIO"]] = None,
+        video_start_timestamp: int = None,
         disable_notification: bool = None,
         reply_parameters: "types.ReplyParameters" = None,
         reply_markup: Union[
@@ -88,6 +90,12 @@ class CopyMessage:
 
             show_caption_above_media (``bool``, *optional*):
                 Pass True, if the caption must be shown above the message media. Ignored if a new caption isn't specified.
+
+            video_cover (``str`` | :obj:`io.BytesIO`, *optional*):
+                New cover for the copied video in the message. Pass None to skip cover uploading and use the existing cover.
+            
+            video_start_timestamp (``int``, *optional*):
+                New start timestamp, from which the video playing must start, in seconds for the copied video in the message.
 
             disable_notification (``bool``, *optional*):
                 Sends the message silently.
@@ -154,6 +162,8 @@ class CopyMessage:
             parse_mode=parse_mode,
             caption_entities=caption_entities,
             show_caption_above_media=show_caption_above_media,
+            video_cover=video_cover,
+            video_start_timestamp=video_start_timestamp,
             disable_notification=disable_notification,
             reply_parameters=reply_parameters,
             reply_markup=reply_markup,

pyrogram/types/messages_and_media/video.py
@@ -147,7 +147,7 @@ class Video(Object):
             cover=types.Photo._parse(
                 client,
                 media.video_cover
-            ) if media.video_cover else None,
-            start_timestamp=media.video_timestamp,
+            ) if media and media.video_cover else None,
+            start_timestamp=media.video_timestamp if media else None,
             client=client
         )

commit 0c95d1236442bc4a1cce60386374fd4652d7a3f4

Fix one mistake in copy_message (DD)
Regression was introduced in b523632f1f

pyrogram/types/messages_and_media/message.py
@@ -4834,7 +4834,9 @@ class Message(Object, Update):
                 send_as=send_as,
                 reply_markup=self.reply_markup if reply_markup is object else reply_markup
             )
-
+            if caption is None:
+                caption = self.caption or ""
+                caption_entities = self.caption_entities
             if self.photo:
                 file_id = self.photo.file_id
             elif self.audio:
@@ -4978,10 +4980,6 @@ class Message(Object, Update):
                 )
             else:
                 raise ValueError("Unknown media type")
-
-            if caption is None:
-                caption = self.caption or ""
-                caption_entities = self.caption_entities
 
             return await send_media(
                 file_id=file_id,