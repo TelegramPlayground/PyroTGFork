ef54631b73915c15baf6a53bfe8723792cb1860b
shriMADhav U k <SpEcHIDe@users.noreply.github.com>
2025-03-07 16:53:50 +0100

    Add paid_message_star_count to Message

--- pyrogram/types/messages_and_media/message.py
+++ pyrogram/types/messages_and_media/message.py
@@ -416,6 +416,9 @@ class Message(Object, Update):
         screenshot_taken (:obj:`~pyrogram.types.ScreenshotTaken`, *optional*):
             A service message that a screenshot of a message in the chat has been taken.
 
+        paid_message_star_count (``int``, *optional*):
+            The number of Telegram Stars the sender paid to send the message.
+
         link (``str``, *property*):
             Generate a link to this message, only for supergroups and channels. Can be None if the message cannot have a link.
 
@@ -547,6 +550,7 @@ class Message(Object, Update):
         contact_registered: "types.ContactRegistered" = None,
         chat_join_type: "enums.ChatJoinType" = None,
         screenshot_taken: "types.ScreenshotTaken" = None,
+        paid_message_star_count: int = None,
         _raw = None
     ):
         super().__init__(client)
@@ -657,6 +661,7 @@ class Message(Object, Update):
         self.contact_registered = contact_registered
         self.chat_join_type = chat_join_type
         self.screenshot_taken = screenshot_taken
+        self.paid_message_star_count = paid_message_star_count
         self._raw = _raw
 
     @staticmethod
@@ -1373,7 +1378,8 @@ class Message(Object, Update):
                 link_preview_options=link_preview_options,
                 effect_id=getattr(message, "effect", None),
                 show_caption_above_media=show_caption_above_media,
-                paid_media=paid_media
+                paid_media=paid_media,
+                paid_message_star_count=message.paid_message_stars
             )
 
             parsed_message.external_reply = await types.ExternalReplyInfo._parse(
