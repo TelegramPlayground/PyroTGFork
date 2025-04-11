5da49e97630a78dfffc12240a1bc6c28941424ea
shriMADhav U k <SpEcHIDe@users.noreply.github.com>
2025-04-11 18:14:39 +0200

    Rename BusinessBotRights

--- pyrogram/types/business/business_bot_rights.py
+++ pyrogram/types/business/business_bot_rights.py
@@ -16,6 +16,8 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Optional
+
 import pyrogram
 from pyrogram import raw
 
@@ -23,64 +25,84 @@ from ..object import Object
 
 
 class BusinessBotRights(Object):
-    """TODO.
+    """Represents the rights of a business bot.
 
     Parameters:
-        can_reply (``bool``):
-            True, if the bot can act on behalf of the business account in chats that were active in the last 24 hours.
+        can_reply (``bool``, *optional*):
+            True, if the bot can send and edit messages in the private chats that had incoming messages in the last 24 hours.
 
-        can_read_messages (``bool``):
+        can_read_messages (``bool``, *optional*):
+            True, if the bot can mark incoming private messages as read.
 
-        can_delete_sent_messages (``bool``):
+        can_delete_outgoing_messages (``bool``, *optional*):
+            True, if the bot can delete messages sent by the bot.
 
-        can_delete_all_messages (``bool``):
+        can_delete_all_messages (``bool``, *optional*):
+            True, if the bot can delete all private messages in managed chats.
 
-        can_edit_name (``bool``):
+        can_edit_name (``bool``, *optional*):
+            True, if the bot can edit the first and last name of the business account.
 
-        can_edit_bio (``bool``):
+        can_edit_bio (``bool``, *optional*):
+            True, if the bot can edit the bio of the business account.
 
-        can_edit_profile_photo (``bool``):
-        can_edit_username (``bool``):
-        can_view_gifts_and_stars (``bool``):
-        can_convert_gifts_to_stars (``bool``):
-        can_change_gift_settings (``bool``):
-        can_transfer_and_upgrade_gifts (``bool``):
-        can_transfer_stars (``bool``):
-        can_manage_stories (``bool``):
+        can_edit_profile_photo (``bool``, *optional*):
+            True, if the bot can edit the profile photo of the business account.
+
+        can_edit_username (``bool``, *optional*):
+            True, if the bot can edit the username of the business account.
+
+        can_change_gift_settings (``bool``, *optional*):
+            True, if the bot can change the privacy settings pertaining to gifts for the business account.
+
+        can_view_gifts_and_stars (``bool``, *optional*):
+            True, if the bot can view gifts and the amount of Telegram Stars owned by the business account.
+
+        can_convert_gifts_to_stars (``bool``, *optional*):
+            True, if the bot can convert regular gifts owned by the business account to Telegram Stars.
+        
+        can_transfer_and_upgrade_gifts (``bool``, *optional*):
+            True, if the bot can transfer and upgrade gifts owned by the business account.
+
+        can_transfer_stars (``bool``, *optional*):
+            True, if the bot can transfer Telegram Stars received by the business account to its own account, or use them to upgrade and transfer gifts.
+
+        can_manage_stories (``bool``, *optional*):
+            True, if the bot can post, edit and delete stories on behalf of the business account.
 
     """
 
     def __init__(
         self,
         *,
-        can_reply: bool = None,
-        can_read_messages: bool = None,
-        can_delete_sent_messages: bool = None,
-        can_delete_all_messages: bool = None,
-        can_edit_name: bool = None,
-        can_edit_bio: bool = None,
-        can_edit_profile_photo: bool = None,
-        can_edit_username: bool = None,
-        can_view_gifts_and_stars: bool = None,
-        can_convert_gifts_to_stars: bool = None,
-        can_change_gift_settings: bool = None,
-        can_transfer_and_upgrade_gifts: bool = None,
-        can_transfer_stars: bool = None,
-        can_manage_stories: bool = None,
+        can_reply: Optional[bool] = None,
+        can_read_messages: Optional[bool] = None,
+        can_delete_outgoing_messages: Optional[bool] = None,
+        can_delete_all_messages: Optional[bool] = None,
+        can_edit_name: Optional[bool] = None,
+        can_edit_bio: Optional[bool] = None,
+        can_edit_profile_photo: Optional[bool] = None,
+        can_edit_username: Optional[bool] = None,
+        can_change_gift_settings: Optional[bool] = None,
+        can_view_gifts_and_stars: Optional[bool] = None,
+        can_convert_gifts_to_stars: Optional[bool] = None,
+        can_transfer_and_upgrade_gifts: Optional[bool] = None,
+        can_transfer_stars: Optional[bool] = None,
+        can_manage_stories: Optional[bool] = None,
     ):
         super().__init__()
 
         self.can_reply = can_reply
         self.can_read_messages = can_read_messages
-        self.can_delete_sent_messages = can_delete_sent_messages
+        self.can_delete_outgoing_messages = can_delete_outgoing_messages
         self.can_delete_all_messages = can_delete_all_messages
         self.can_edit_name = can_edit_name
         self.can_edit_bio = can_edit_bio
         self.can_edit_profile_photo = can_edit_profile_photo
         self.can_edit_username = can_edit_username
+        self.can_change_gift_settings = can_change_gift_settings
         self.can_view_gifts_and_stars = can_view_gifts_and_stars
         self.can_convert_gifts_to_stars = can_convert_gifts_to_stars
-        self.can_change_gift_settings = can_change_gift_settings
         self.can_transfer_and_upgrade_gifts = can_transfer_and_upgrade_gifts
         self.can_transfer_stars = can_transfer_stars
         self.can_manage_stories = can_manage_stories
@@ -96,15 +118,15 @@ class BusinessBotRights(Object):
         return BusinessBotRights(
             can_reply=business_bot_rights.reply,
             can_read_messages=business_bot_rights.read_messages,
-            can_delete_sent_messages=business_bot_rights.delete_sent_messages,
+            can_delete_outgoing_messages=business_bot_rights.delete_sent_messages,
             can_delete_all_messages=business_bot_rights.delete_received_messages,
             can_edit_name=business_bot_rights.edit_name,
             can_edit_bio=business_bot_rights.edit_bio,
             can_edit_profile_photo=business_bot_rights.edit_profile_photo,
             can_edit_username=business_bot_rights.edit_username,
+            can_change_gift_settings=business_bot_rights.change_gift_settings,
             can_view_gifts_and_stars=business_bot_rights.view_gifts,
             can_convert_gifts_to_stars=business_bot_rights.sell_gifts,
-            can_change_gift_settings=business_bot_rights.change_gift_settings,
             can_transfer_and_upgrade_gifts=business_bot_rights.transfer_and_upgrade_gifts,
             can_transfer_stars=business_bot_rights.transfer_stars,
             can_manage_stories=business_bot_rights.manage_stories,
