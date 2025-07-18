67163d49ef5775b998193f84bee15d5256c2b4cd
shriMADhav U k <SpEcHIDe@users.noreply.github.com>
2025-07-18 07:32:08 +0200

    (fix): AttributeError

--- pyrogram/methods/messages/send_paid_media.py
+++ pyrogram/methods/messages/send_paid_media.py
@@ -337,7 +337,7 @@ class SendPaidMedia:
         session = None
         business_connection = None
         if business_connection_id:
-            business_connection = self.business_user_connection_cache.get(business_connection_id)
+            business_connection = self.business_user_connection_cache[business_connection_id]
             if business_connection is None:
                 business_connection = await self.get_business_connection(business_connection_id)
             session = await get_session(
