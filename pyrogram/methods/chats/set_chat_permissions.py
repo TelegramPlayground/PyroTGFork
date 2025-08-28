e3171d51ac1f9d1060a3a3d9029f05dd1548a93d
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-08-28 10:18:33 +0530

    Update set_chat_permissions.py

--- pyrogram/methods/chats/set_chat_permissions.py
+++ pyrogram/methods/chats/set_chat_permissions.py
@@ -54,12 +54,12 @@ class SetChatPermissions:
                 # Completely restrict chat
                 await app.set_chat_permissions(chat_id, ChatPermissions())
 
-                # Chat members can only send text messages and media messages
+                # Chat members can only send text messages and documents
                 await app.set_chat_permissions(
                     chat_id,
                     ChatPermissions(
                         can_send_messages=True,
-                        can_send_media_messages=True
+                        can_send_documents=True
                     )
                 )
         """
@@ -67,20 +67,7 @@ class SetChatPermissions:
         r = await self.invoke(
             raw.functions.messages.EditChatDefaultBannedRights(
                 peer=await self.resolve_peer(chat_id),
-                banned_rights=raw.types.ChatBannedRights(
-                    until_date=0,
-                    send_messages=not permissions.can_send_messages,
-                    send_media=not permissions.can_send_media_messages,
-                    send_stickers=not permissions.can_send_other_messages,
-                    send_gifs=not permissions.can_send_other_messages,
-                    send_games=not permissions.can_send_other_messages,
-                    send_inline=not permissions.can_send_other_messages,
-                    embed_links=not permissions.can_add_web_page_previews,
-                    send_polls=not permissions.can_send_polls,
-                    change_info=not permissions.can_change_info,
-                    invite_users=not permissions.can_invite_users,
-                    pin_messages=not permissions.can_pin_messages,
-                )
+                banned_rights=permissions.write(False)
             )
         )
 
