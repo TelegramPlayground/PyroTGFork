a841430c0d177c6e36727b7c3640225bf92487bf
shriMADhav U k <hetzner@shrimadhavuk.me>
2026-03-22 16:56:46 +0100

    fix tests

--- tests/parser/test_markdown.py
+++ tests/parser/test_markdown.py
@@ -42,6 +42,24 @@ def test_markdown_unparse_italic():
     assert Markdown.unparse(text=text, entities=entities) == expected
 
 
+def test_markdown_unparse_italic_html():
+    expected = "__This works, it's ok__ <b>This shouldn't</b>"
+    text = "This works, it's ok <b>This shouldn't</b>"
+    entities = pyrogram.types.List(
+        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=0, length=19)])
+
+    assert Markdown.unparse(text=text, entities=entities) == expected
+
+
+def test_markdown_unparse_underline():
+    expected = "--underline--"
+    text = "underline"
+    entities = pyrogram.types.List(
+        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.UNDERLINE, offset=0, length=9)])
+
+    assert Markdown.unparse(text=text, entities=entities) == expected
+
+
 def test_markdown_unparse_strike():
     expected = "~~strike~~"
     text = "strike"
@@ -72,8 +90,11 @@ def test_markdown_unparse_url():
 def test_markdown_unparse_emoji():
     expected = '![🥲](tg://emoji?id=5195264424893488796) im crying'
     text = "🥲 im crying"
-    entities = pyrogram.types.List([pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.CUSTOM_EMOJI,
-                                                                 offset=0, length=2, custom_emoji_id=5195264424893488796)])
+    entities = pyrogram.types.List([
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.CUSTOM_EMOJI,
+        offset=0, length=2,
+        custom_emoji_id=5195264424893488796)
+    ])
 
     assert Markdown.unparse(text=text, entities=entities) == expected
 
@@ -90,43 +111,53 @@ def test_markdown_unparse_code():
 def test_markdown_unparse_pre():
     expected = """```python
 for i in range(10):
-    print(i)
-```"""
+    print(i)```"""
 
     text = """for i in range(10):
     print(i)"""
-
-    entities = pyrogram.types.List([pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.PRE, offset=0,
-                                                                 length=32, language='python')])
+    entities = pyrogram.types.List([
+        pyrogram.types.MessageEntity(
+            type=pyrogram.enums.MessageEntityType.PRE,
+            offset=0, length=32,
+            language="python"
+        )
+    ])
 
     assert Markdown.unparse(text=text, entities=entities) == expected
 
 
 def test_markdown_unparse_blockquote():
-    expected = """> Hello
-> from
+    expected = """>Block quotation started
+>Block quotation continued
+>The last line of the block quotation
+**>Expandable block quotation started
+>Expandable block quotation continued
+>Expandable block quotation continued
+>Hidden by default part of the block quotation started
+>Expandable block quotation continued
+>The last line of the block quotation||"""
 
-> pyrogram!"""
+    text = """Block quotation started\nBlock quotation continued\nThe last line of the block quotation\nExpandable block quotation started\nExpandable block quotation continued\nExpandable block quotation continued\nHidden by default part of the block quotation started\nExpandable block quotation continued\nThe last line of the block quotation"""
 
-    text = """Hello\nfrom\n\npyrogram!"""
-
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BLOCKQUOTE, offset=0, length=10),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BLOCKQUOTE, offset=12, length=9)])
+    entities = pyrogram.types.List([
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BLOCKQUOTE, offset=0, length=86),
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.EXPANDABLE_BLOCKQUOTE, offset=87, length=236)
+    ])
 
     assert Markdown.unparse(text=text, entities=entities) == expected
 
 
 def test_markdown_unparse_mixed():
-    expected = "**aaaaaaa__aaabbb**__~~dddddddd||ddeee~~||||eeeeeeefff||ffff`fffggggggg`ggghhhhhhhhhh"
+    expected = "**aaaaaaa__aaabbb__**~~dddddddd||ddeee||~~||eeeeeeefff||ffff`fffggggggg`ggghhhhhhhhhh"
     text = "aaaaaaaaaabbbddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhh"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BOLD, offset=0, length=13),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=7, length=6),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.STRIKETHROUGH, offset=13, length=13),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=21, length=5),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=26, length=10),
-         pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.CODE, offset=40, length=10)])
+    entities = pyrogram.types.List([
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.BOLD, offset=0, length=13),
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=7, length=6),
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.STRIKETHROUGH, offset=13, length=13),
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=21, length=5),
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.SPOILER, offset=26, length=10),
+        pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.CODE, offset=40, length=10)
+    ])
 
     assert Markdown.unparse(text=text, entities=entities) == expected
 
@@ -135,13 +166,5 @@ def test_markdown_unparse_no_entities():
     expected = "text"
     text = "text"
     entities = []
-
-    assert Markdown.unparse(text=text, entities=entities) == expected
-
-def test_markdown_unparse_html():
-    expected = "__This works, it's ok__ <b>This shouldn't</b>"
-    text = "This works, it's ok <b>This shouldn't</b>"
-    entities = pyrogram.types.List(
-        [pyrogram.types.MessageEntity(type=pyrogram.enums.MessageEntityType.ITALIC, offset=0, length=19)])
 
     assert Markdown.unparse(text=text, entities=entities) == expected
